57090cda12a9b45445a1e0684335d4e10a1741c0
Rahul H <152089501+RahulH007@users.noreply.github.com>
2025-03-12 15:39:13 +0530

    Add files via upload

---
+++ Program_1/Program_1b.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        int boardSize = 8;
+
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("O ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
