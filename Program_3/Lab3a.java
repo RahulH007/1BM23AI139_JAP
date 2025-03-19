8855f35eac077e91720fa5476ccd687d4bb0228b
Rahul H <152089501+RahulH007@users.noreply.github.com>
2025-03-19 15:28:23 +0530

    Add files via upload

---
+++ Program_3/Lab3a.java
@@ -0,0 +1,25 @@
+//public class Lab3a
+//{
+//    public static String concatenate(String s1,String s2){
+//        return s1+s2;
+//    }
+//    public static boolean isequals(String s1, String s2){
+//        return s1.equals(s2);
+//    }
+//    public static String copy(String s1){
+//        return new String(s1);
+//    }
+//
+//    public static void main(String args[])
+//    {
+//        String s1="Hello";
+//        String s2="World";
+//
+//        System.out.println("String Comparison");
+//        System.out.println(("are the strings equal "+isequals(s1,s2)));
+//        System.out.println("Concatenating the Strings "+ concatenate(s1,s2));
+//        String copied =copy(s1);
+//        System.out.println("Copied String is "+ copied);
+//
+//    }
+//}
